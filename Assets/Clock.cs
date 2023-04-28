30fe4abcff6e9ec034fc0be2bf20fb3dc98ab6de
AndyLovett <127253295+lordlovett@users.noreply.github.com>
2023-04-28 11:05:07 -0400

    Exercice 6

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Clock : MonoBehaviour
 {
@@ -11,9 +12,13 @@ public class Clock : MonoBehaviour
 
 	private int _minutePerHour = 60;
 	private int _secondPerMinute = 60;
-
+	
+	private UnityEvent _crossoverEvent = new UnityEvent();
+	private bool _crossoverInvoked = false;
+	
 	void Start()
 	{
+		_crossoverEvent.AddListener(OnCrossOver);
 		SetHands(DateTime.Now);
 	}
 
@@ -24,10 +29,31 @@ public class Clock : MonoBehaviour
 
 	void SetHands(DateTime time)
 	{
+		time = time.AddHours(1);
 		float degHour = _degPerHour * time.Hour + _degPerHour / _minutePerHour * time.Minute;
 		float degMinute = (_degPerMinute * time.Minute) + (_degPerMinute / _secondPerMinute) * time.Second ;
 		
 		_hourPivot.localRotation = Quaternion.Euler(0, degHour, 0);
 		_minutePivot.localRotation = Quaternion.Euler(0, degMinute, 0);
+
+		float delta = degHour - degMinute;
+		if (delta > 0f && delta < Mathf.Epsilon)
+		{
+			if (!_crossoverInvoked)
+			{
+				_crossoverEvent?.Invoke();
+			}
+		}
+		// Avoid invoking every frame while minute hand crosses over hour hand
+		if (_crossoverInvoked && delta < 0.5f)
+		{
+			_crossoverInvoked = false;
+		}
+	}
+
+	private void OnCrossOver()
+	{
+		Debug.LogFormat("Minute Hand passed over Hour Hand.");
+		_crossoverInvoked = true;
 	}
 }
